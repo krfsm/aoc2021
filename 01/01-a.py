8fabe6088f82b333eed28757aadc2e6d10fe57f4
cd <cd.skogsberg@gmail.com>
2021-12-01 20:33:58 +0100

    improvement

--- 01/01-a.py
+++ 01/01-a.py
@@ -3,10 +3,10 @@
 total_measurements = 0
 
 with open('input-1.txt', 'r') as f:
-    inputs = f.read().splitlines()
+    inputs = [int(x) for x in f.read().splitlines()]
 
 for i in range(1, len(inputs)):
-    if int(inputs[i]) > int(inputs[i-1]):
+    if inputs[i] > inputs[i-1]:
         total_measurements = total_measurements+1
 
 print('increases:', total_measurements)
